80eee140db85d1f3775850f103faebf482161352
JoaoPedroLage <joaopedro.lage@outlook.com>
2023-05-12 21:26:24 -0300

    feat: configures the api file of the routes to return the json of the activities of the post method

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,31 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Carregar a classe do controlador
+require_once('../app/Http/Controllers/ActivitiesController.php');
+
+// Criar uma instância do controlador
+$activitiesController = new ActivitiesController();
+
+Route::post('/atividades', function (Request $request) use ($activitiesController) {
+  // Chamar o método index do controlador para processar a requisição POST
+  $data = $activitiesController->index($request);
+
+  // Retornar a view 'api' com os dados
+  return view('api')->with('data', $data);
+});
+
+
+// Criar uma instância do controlador
+$activitiesController = new ActivitiesController();
+
+// Verificar o método da requisição
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  // Chamar o método index do controlador para processar a requisição POST
+  $request = Request::createFromGlobals();
+  $activitiesController->index($request);
+} else {
+  // Retornar um erro para requisições que não sejam POST
+  header('HTTP/1.1 405 Method Not Allowed');
+}
